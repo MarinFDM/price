a63c1389bb102cce66bb9b8da27778df6ab95a80
Marin <marinn@morlhon.net>
2021-12-14 15:20:44 +0100

    connection api prix

--- price/src/App.vue
+++ price/src/App.vue
@@ -1,21 +1,82 @@
-<script setup>
+<script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
+  import axios from 'axios';
+  export default {
+  name: "App",
+  components: {},
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://blockchain.info/ticker')
+      .then(response => (this.info = response))
+  }
+
+};
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello marin" />
+  <table class="table table-striped">
+     <caption>Etherum</caption>
+        <thead>
+           <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Actions</th>
+           </tr>
+        </thead>
+          <tbody>
+              <tr>
+                 <td>toto</td>
+                 <td>toto1</td>
+                 <td>toto2</td>
+                 <td>toto3</td>
+               </tr>
+                <tr>
+                  <td>toto4</td>
+                  <td>toto5</td>
+                  <td>toto6</td>
+                  <td>toto7</td>
+               </tr>
+               <tr>
+                 <td>toto8</td>
+                 <td>toto9</td>
+                 <td>toto10</td>
+                 <td>toto11</td>
+                </tr>
+            </tbody>
+   </table>
+
+   <div>
+  {{ info }}
+</div>
+   
 </template>
 
 <style>
+td
+{
+ border: 10px solid black;
+}
+
+table
+{ 
+  border-collapse: collapse;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: futura, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
   margin-top: 60px;
 }
 </style>
